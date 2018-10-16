74df9db357de18d9fd47367b1512b81a4221b320
Jake Valladares <jason.valladares3@gmail.com>
2018-10-16 12:46:23 -0400

    initial commit

---
+++ arduinotest1.py
@@ -0,0 +1,17 @@
+#!/user/bin/env python
+
+import serial
+import sys
+port = "/dev/ttyACM0"
+rate = 9600
+
+s1 = serial.Serial(port,rate)
+s1.flushInput()
+
+print("begin waiting")
+s1.read_until()
+print("Enter a number to square")
+x = input()
+
+s1.write(str(x).encode('utf-8'))
+print(s1.read_until())
