f9a7d1fa137a26bae688e24c07ede50b35a1c2a4
Timothy McNamara <timmy.mc.2@gmail.com>
2018-11-01 19:12:33 -0400

    Added code for all 4 photoreflectors

---
+++ arduinocode/Main/Photoreflectors.ino
@@ -0,0 +1,53 @@
+#include "./PinChangeInt/PinChangeInt.h"
+
+#define PinMotor1Sensor1 2
+#define PinMotor1Sensor2 3
+#define PinMotor2Sensor1 6
+#define PinMotor2Sensor2 7
+
+volatile long right_count = 0;
+volatile long left_count = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  //Attach Interrupts
+//  attachInterrupt(digitalPinToInterrupt(PinMotor1Sensor1), right_encoder_isr, CHANGE);
+//attachInterrupt(digitalPinToInterrupt(PinMotor1Sensor1), right_encoder_isr, CHANGE);
+//attachInterrupt(digitalPinToInterrupt(PinMotor1Sensor2), right_encoder_isr, CHANGE);
+  attachPinChangeInterrupt(PinMotor1Sensor1, right_encoder_isr, CHANGE);
+  attachPinChangeInterrupt(PinMotor1Sensor2, right_encoder_isr, CHANGE);
+//  attachInterrupt(digitalPinToInterrupt(PinMotor2Sensor1), left_encoder_isr, CHANGE);
+  attachPinChangeInterrupt(PinMotor2Sensor1, left_encoder_isr, CHANGE);
+  attachPinChangeInterrupt(PinMotor2Sensor2, left_encoder_isr, CHANGE);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+void right_encoder_isr() {
+    static int8_t lookup_table_r[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
+    static uint8_t enc_val_r = 0;
+    enc_val_r = enc_val_r << 2;
+    enc_val_r = enc_val_r | ((PIND & 0b00001100) >> 2);
+    right_count = right_count + lookup_table_r[enc_val_r & 0b1111];
+
+    Serial.println("Right: " + String(right_count));
+    Serial.println("Left: " + String(left_count));
+    Serial.println();
+}
+
+void left_encoder_isr() {
+    static int8_t lookup_table_l[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
+    static uint8_t enc_val_l = 0;
+    enc_val_l = enc_val_l << 2;
+    enc_val_l = enc_val_l | ((PIND & 0b11000000) >> 6);
+//    Serial.println(enc_val_l);
+    left_count = left_count + lookup_table_l[enc_val_l & 0b1111];
+
+    Serial.println("Right: " + String(right_count));
+    Serial.println("Left: " + String(left_count));
+    Serial.println();
+}
