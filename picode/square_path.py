18c81e5cc981eb5fb6d9759f40f90863871fd1a8
Jason Valladares <jasonvallada@umass.edu>
2018-11-09 17:00:35 -0500

    ..

--- picode/square_path.py
+++ picode/square_path.py
@@ -83,7 +83,7 @@ class robot:
             print("end measurement")
             l_trans, r_trans = self.get_actual_translation()
             t = time.time()
-            v = (l_trans - l_trans_start)/(t - start_time)
+            v = (r_trans - r_trans_start)/(t - start_time)
             print("v is " + str(v))
             velocities[i] = v
 
@@ -106,7 +106,7 @@ class robot:
             print("end measurement")
             l_trans, r_trans = self.get_actual_translation()
             t = time.time()
-            v = (r_trans - r_trans_start)/(t - start_time)
+            v = (l_trans - l_trans_start)/(t - start_time)
             print("v is " + str(v))
             velocities[i] = v
 
