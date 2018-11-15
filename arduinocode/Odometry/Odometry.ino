a1e58d5d6d318538b446461e5abb0d62eba03385
pi <jason.valladares3@gmail.com>
2018-11-15 11:23:30 -0500

    odometry

--- arduinocode/Odometry/Odometry.ino
+++ arduinocode/Odometry/Odometry.ino
@@ -15,19 +15,20 @@ double B = 1.0;
 double N_enc = 32.0; // Segments on Encoder = 32
 double dia = 0.071; // Dia = 71 mm
 double WB = 0.157;  // Wheel Base = 157 mm
-double w = 10.0; // Weighting factor
+double w1 = 0.5; // Weighting factor right
+double w2 = 1.0; //weighting factor left
 int flag = 0;
 
 // Parameters:
-double PWM_ref = 200;
-double PWM_l = PWM_ref;
-double PWM_r = PWM_ref;
+double PWM_l;
+double PWM_r;
 
 // Variables:
 double right_count = 0.0;
 double left_count = 0.0;
 double err_count = 0.0;
-double n_l_ref = 0.0, n_r_ref = 0.0;
+double n_l_ref = 0.0; 
+double n_r_ref = 0.0;
 double del_x, x, y, theta = 0.0;
 double S_l = 0.0, S_r = 0.0;
 double loc[3];
@@ -45,14 +46,17 @@ void setup()
 
   attachPinChangeInterrupt(PinMotor2Sensor1, left_encoder_isr, CHANGE);
   attachPinChangeInterrupt(PinMotor2Sensor2, left_encoder_isr, CHANGE);
+
 }
 
 void loop()
 {
-  left_encoder_isr();
-  right_encoder_isr();
   first_stretch();
-  //motor_control();
+  //else if (flag == 1)
+  //{
+  //curve_path();
+  //}
+  //curve_path();
   //Serial.println("Right:" + String(left_count) + " Left:" + String(right_count) + "; S_l=" + String(S_l) + " S_r=" + String(S_r) + "; theta:" + String(theta * (180.0 / M_PI)));
   //Serial.println(Etime);
   delay(100);
@@ -67,11 +71,14 @@ void loop()
   Serial.print("  PWM_l:");
   Serial.print(PWM_l);
   Serial.print("  PWM_r:");
-  Serial.println(PWM_r);
+  Serial.print(PWM_r);
+  Serial.print("  err_count:");
+  Serial.println(err_count);
 }
 
 void right_encoder_isr() {
-  static int8_t lookup_table_r[] = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
+  static int8_t lookup_table_r[] = {
+    0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0    };
   static uint8_t enc_val_r = 0;
   enc_val_r = enc_val_r << 2;
   enc_val_r = enc_val_r | ((PIND & 0b00001100) >> 2);
@@ -79,7 +86,8 @@ void right_encoder_isr() {
 }
 
 void left_encoder_isr() {
-  static int8_t lookup_table_l[] = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
+  static int8_t lookup_table_l[] = {
+    0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0    };
   static uint8_t enc_val_l = 0;
   enc_val_l = enc_val_l << 2;
   enc_val_l = enc_val_l | ((PIND & 0b01100000) >> 5);
@@ -88,6 +96,9 @@ void left_encoder_isr() {
 }
 
 void get_location() {
+  left_encoder_isr();
+  right_encoder_isr();
+  err_count = abs(right_count - left_count);
   S_l = ((M_PI * dia * left_count) / N_enc);
   S_r = ((M_PI * dia * right_count) / N_enc);
   theta = atan2(((S_r - S_l) / 2.0), (WB / 2.0));
@@ -99,32 +110,24 @@ void get_location() {
   loc[2] = theta;
 }
 
+/*
 void set_refcounts() {
-  n_l_ref = (N_enc * S_l_ref) / (M_PI * dia);
-  n_r_ref = (N_enc * S_r_ref) / (M_PI * dia);
-}
+ n_l_ref = (N_enc * S_l_ref) / (M_PI * dia);
+ n_r_ref = (N_enc * S_r_ref) / (M_PI * dia);
+ }
+ */
 
 void motor_control() {
-  get_location();
-  if ((S_l < S_l_ref) && (S_r < S_r_ref)) {
+  if ((S_l < S_l_ref) || (S_r < S_r_ref)) {
     throttle();
   }
   else {
     brake();
-    S_l_ref = 0;
-    S_r_ref = 0;
+    Serial.end();
   }
 }
 
 void throttle() {
-   left_encoder_isr();
-   right_encoder_isr();
-    if (left_count > right_count) {
-    PWM_l = PWM_l - w * (err_count);
-    }
-    else if (right_count > left_count) {
-    PWM_r = PWM_r - w * (err_count);
-    }
   md.setM1Speed(PWM_l); //Left
   md.setM2Speed(PWM_r); //Right
 }
@@ -137,25 +140,34 @@ void brake() {
 void first_stretch() {
   PWM_l = 200;
   PWM_r = 200;
-  S_l_ref = 0.6096;
-  S_r_ref = 0.6096;
+  S_l_ref = 1.0;
+  S_r_ref = 1.0;
+  get_location();
+  if (err_count < 0.0) {
+    PWM_l = PWM_l - w1 * (err_count);
+  }
+  else if (err_count > 0.0) {
+    PWM_l = PWM_l + w2 * (err_count);
+  }
   motor_control();
-  flag = 1;
 }
 
 void curve_path() {
-  PWM_l = 100;
-  PWM_r = 200;
-  S_l_ref = 0.83;
-  S_r_ref = 0.57;
+  flag = 2;
+  PWM_l = 200;
+  PWM_r = 300;
+  S_l_ref = 0.83/2;
+  S_r_ref = 0.57/2;
   /*
     if (left_count > right_count) {
-    PWM_l = PWM_l - w * (err_count);
-    }
-    else if (right_count > left_count) {
-    PWM_r = PWM_r - w * (err_count);
-    }
-  */
+   PWM_l = PWM_l - w * (err_count);
+   }
+   else if (right_count > left_count) {
+   PWM_r = PWM_r - w * (err_count);
+   }
+   */
+  md.setM1Speed(PWM_l); //Left
+  md.setM2Speed(PWM_r); //Right
   motor_control();
   flag = 2;
 }
@@ -164,14 +176,16 @@ void second_stretch() {
   S_l_ref = 2 * 0.6096;
   S_r_ref = 2 * 0.6096;
   get_location();
-  set_refcounts();
+  //set_refcounts();
   /*
     if (left_count > right_count) {
-    PWM_l = PWM_l - w * (err_count);
-    }
-    else if (right_count > left_count) {
-    PWM_r = PWM_r - w * (err_count);
-    }
-  */
+   PWM_l = PWM_l - w * (err_count);
+   }
+   else if (right_count > left_count) {
+   PWM_r = PWM_r - w * (err_count);
+   }
+   */
   motor_control();
 }
+
+
