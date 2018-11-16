7ce2d29cd317c15f5a2c7294b850e0c5b45bcea3
Soumitra <soumitra.sitole@gmail.com>
2018-11-15 20:48:23 -0500

    updated main file with odometry location

--- arduinocode/Main/Odometry_Location.ino
+++ arduinocode/Main/Odometry_Location.ino
@@ -1,8 +1,42 @@
+#include "math.h"
 
+extern volatile long left_count;
 extern volatile long right_count;
 
-void printstuff()
+int N_enc = 32;
+double dia = 0.071; // Dia = 71 mm
+double WB = 0.157;
+
+double S_l;
+double S_r;
+double del_x;
+double x;
+double y;
+double theta;
+double loc[3];
+
+void get_location()
 {
-  Serial.println("hello");
-  Serial.println(right_count);
+  S_l = (M_PI*left_count*dia)/N_enc;
+  S_r = (M_PI*right_count*dia)/N_enc;
+  theta = atan2(((S_r - S_l) / 2.0), (WB / 2.0));
+  del_x = (S_l + S_r) / 2;
+  x = del_x * cos(theta);
+  y = del_x * sin(theta);
+  loc[0] = x;
+  loc[1] = y;
+  loc[2] = theta;
+  Serial.print("  x:");
+  Serial.print(x);
+  Serial.print("  y:");
+  Serial.print(y);
+  Serial.print("  n_l:");
+  Serial.print(left_count);
+  Serial.print("  n_r:");
+  Serial.print(right_count);
+  Serial.print("  err_count:");
+  Serial.print("  S_l:");
+  Serial.print(S_l);
+  Serial.print("  S_r:");
+  Serial.println(S_r);
 }
